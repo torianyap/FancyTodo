d1fc0aa87ff94d3f2068e3c020e45b2a49421579
torianyap <torianyap@gmail.com>
2020-11-02 16:01:56 +0700

    rewrite config.json

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-
 const express = require('express')
 const cors = require('cors')
 const router = require('./routes')
